f0b717e6e13709feb62634901fb538c37f0b604a
Eric Horodyski <horodyski@ionic.io>
2021-05-13 07:19:29 -0400

    Repro Issue

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,43 @@
-import './ExploreContainer.css';
+import { IonItem, IonLabel, IonSelect, IonSelectOption } from "@ionic/react";
+import React, { useState } from "react";
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [toppings, setToppings] = useState<string[]>([]);
+
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonItem>
+      <IonLabel>Toppings</IonLabel>
+      <IonSelect
+        value={toppings}
+        cancelText="Nah"
+        okText="Okay!"
+        onIonChange={(e) => setToppings(e.detail.value)}
+      >
+        <IonSelectOption value="bacon">Bacon</IonSelectOption>
+        <IonSelectOption value="olives">Black Olives</IonSelectOption>
+        <IonSelectOption value="xcheese">Extra Cheese</IonSelectOption>
+        <IonSelectOption value="peppers">Green Peppers</IonSelectOption>
+        <IonSelectOption value="mushrooms">Mushrooms</IonSelectOption>
+        <IonSelectOption value="onions">Onions</IonSelectOption>
+        <IonSelectOption value="pepperoni">Pepperoni</IonSelectOption>
+        <IonSelectOption value="pineapple">Pineapple</IonSelectOption>
+        <IonSelectOption value="sausage">Sausage</IonSelectOption>
+        <IonSelectOption value="Spinach">Spinach</IonSelectOption>
+        <IonSelectOption value="bacon">Bacon</IonSelectOption>
+        <IonSelectOption value="olives">Black Olives</IonSelectOption>
+        <IonSelectOption value="xcheese">Extra Cheese</IonSelectOption>
+        <IonSelectOption value="peppers">Green Peppers</IonSelectOption>
+        <IonSelectOption value="mushrooms">Mushrooms</IonSelectOption>
+        <IonSelectOption value="onions">Onions</IonSelectOption>
+        <IonSelectOption value="pepperoni">Pepperoni</IonSelectOption>
+        <IonSelectOption value="pineapple">Pineapple</IonSelectOption>
+        <IonSelectOption value="sausage">Sausage</IonSelectOption>
+        <IonSelectOption value="Spinach">Spinach</IonSelectOption>
+      </IonSelect>
+    </IonItem>
   );
 };
 
